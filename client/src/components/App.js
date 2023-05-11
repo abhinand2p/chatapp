4cafc8549a3a5a5dd349e0fe27a8c5c20a1337e6
Abhinand <abhinand2p@gmail.com>
2023-05-12 00:16:12 +0530

    conversation context and sidebar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import Login from './Login'
 import useLocalStorage from '../hooks/useLocalStorage';
 import Dashboard from './Dashboard';
 import { ContactsProvider } from '../contexts/ContactsProvider';
+import { ConversationsProvider } from '../contexts/ConversationsProvider';
 
 function App() {
 
@@ -10,7 +11,9 @@ function App() {
 
   const dashboard = (
     <ContactsProvider>
+      <ConversationsProvider>
       <Dashboard id={id}/>
+      </ConversationsProvider>
     </ContactsProvider>
   )
   return (
